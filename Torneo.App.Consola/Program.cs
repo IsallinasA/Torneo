7225eae8da200a4de07ee3b89cf5c3f18347e7e9
IsallinasA <isabellallinasalvarado@gmail.com>
2022-09-03 22:33:22 -0500

    Consola Clean

--- Torneo.App.Consola/Program.cs
+++ Torneo.App.Consola/Program.cs
@@ -2,15 +2,9 @@
 using Torneo.App.Persistencia;
 namespace Torneo.App.Consola {
 class Program {
-private static IRepositorioMunicipio _repoMunicipio = new RepositorioMunicipio();
+
     static void Main(string[] args) {
-        AddMunicipio();
-    }
-        private static void AddMunicipio() {
-            var municipio = new Municipio {
-            Nombre = "Manizales",
-            };
-            _repoMunicipio.AddMunicipio(municipio);
+
         }
     }
 }
