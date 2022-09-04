843d62741d03af4ed79e024a656a22449f043218
IsallinasA <isabellallinasalvarado@gmail.com>
2022-09-04 01:41:03 -0500

    Consola AddDT

--- Torneo.App.Consola/Program.cs
+++ Torneo.App.Consola/Program.cs
@@ -2,9 +2,38 @@
 using Torneo.App.Persistencia;
 namespace Torneo.App.Consola {
 class Program {
-private static IRepositorioMunicipio _repoMunicipio = new RepositorioMunicipio();
-    static void Main(string[] args) { 
-    }
+private static IRepositorioDT _repoDT = new RepositorioDT();
+    static void Main(string[] args) {
+        int opcion=0;
 
+        do {
+            Console.WriteLine("2. Insertar Director Tecnico");
+            Console.WriteLine("0. Salir");
+            opcion=Int32.Parse(Console.ReadLine());
+            switch(opcion){
+                case 2:
+                    AddDT();
+                    break;
+            }
+        }
+        while(opcion!=0); 
+
+        
+    }
+        private static void AddDT() {
+            Console.WriteLine("Ingrese el nombre del Director Tecnico");
+            string nombre=Console.ReadLine();
+            Console.WriteLine("Ingrese el documento del Director Tecnico");
+            string documento=Console.ReadLine();
+            Console.WriteLine("Ingrese el telefono del Director Tecnico");
+            string telefono=Console.ReadLine();
+            
+            var directorTecnico = new DirectorTecnico {
+            Nombre = nombre,
+            Documento=documento,
+            Telefono=telefono,
+            };
+            _repoDT.AddDT(directorTecnico);
+        }
     }
 }
---
+++ Torneo.App.Persistencia/AppRepositorios/IRepositorioDt.cs
@@ -0,0 +1,6 @@
+using Torneo.App.Dominio;
+namespace Torneo.App.Persistencia {
+public interface IRepositorioDT{
+public DirectorTecnico AddDT (DirectorTecnico directorTecnico);
+    }
+}
