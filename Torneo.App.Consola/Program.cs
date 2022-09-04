19399ceac4b4c154fcc1bc54194a93573fde6b62
IsallinasA <isabellallinasalvarado@gmail.com>
2022-09-04 00:54:57 -0500

    Consola Municipio

--- Torneo.App.Consola/Program.cs
+++ Torneo.App.Consola/Program.cs
@@ -3,30 +3,8 @@ using Torneo.App.Persistencia;
 namespace Torneo.App.Consola {
 class Program {
 private static IRepositorioMunicipio _repoMunicipio = new RepositorioMunicipio();
-    static void Main(string[] args) {
-        int opcion=0;
-
-        do {
-            Console.WriteLine("1. Insertar Municipio");
-            Console.WriteLine("0. Salir");
-            opcion=Int32.Parse(Console.ReadLine());
-            switch(opcion){
-                case 1:
-                    AddMunicipio();
-                    break;
-            }
-        }
-        while(opcion!=0); 
-
+    static void Main(string[] args) { 
     }
-        private static void AddMunicipio() {
-            Console.WriteLine("Ingrese el nombre del Municipio");
-            string nombre=Console.ReadLine();
-            
-            var municipio = new Municipio {
-            Nombre = nombre,
-            };
-            _repoMunicipio.AddMunicipio(municipio);
-        }
+
     }
 }
