be11ac9d2de0e2670ca185ef5b11119277d75d0d
IsallinasA <isabellallinasalvarado@gmail.com>
2022-09-03 22:15:36 -0500

    Consola AddMunicipio

---
+++ Torneo.App.Consola/Program.cs
@@ -0,0 +1,16 @@
+﻿using Torneo.App.Dominio;
+using Torneo.App.Persistencia;
+namespace Torneo.App.Consola {
+class Program {
+private static IRepositorioMunicipio _repoMunicipio = new RepositorioMunicipio();
+    static void Main(string[] args) {
+        AddMunicipio();
+    }
+        private static void AddMunicipio() {
+            var municipio = new Municipio {
+            Nombre = "Manizales",
+            };
+            _repoMunicipio.AddMunicipio(municipio);
+        }
+    }
+}
