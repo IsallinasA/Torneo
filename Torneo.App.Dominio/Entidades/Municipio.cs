e96847d047d68b2e8e8b1c1fd79aded51d862cc8
IsallinasA <isabellallinasalvarado@gmail.com>
2022-09-03 19:54:46 -0500

    Firs Commit

---
+++ Torneo.App.Dominio/Entidades/Municipio.cs
@@ -0,0 +1,6 @@
+namespace Torneo.App.Dominio{
+public class Municipio{
+public int Id { get; set; }
+public string Nombre { get; set; }
+    }
+}
---
+++ Torneo.App.Dominio/Entidades/Posicion.cs
@@ -0,0 +1,6 @@
+namespace Torneo.App.Dominio{
+public class Posicion{
+public int Id { get; set; }
+public string Nombre { get; set; }
+    }
+}
