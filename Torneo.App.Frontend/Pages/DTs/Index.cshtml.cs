6c81f551b14b6af49cbc0ee53f1f0bb99a75aa35
“IsallinasA” <isabellallinasalvarado@gmail.com>
2022-09-11 19:48:12 -0500

    Page IndexDTs

---
+++ Torneo.App.Frontend/Pages/DTs/Index.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Torneo.App.Persistencia;
+using Torneo.App.Dominio;
+
+namespace Torneo.App.Frontend.Pages.DTs{
+    public class IndexModel : PageModel{
+        private readonly IRepositorioDT _repoDT;
+        public IEnumerable<DirectorTecnico> dts { get; set; } //atribbuto
+        public IndexModel(IRepositorioDT repoDT){
+            _repoDT = repoDT;
+        }
+        public void OnGet(){
+            dts = _repoDT.GetAllDTs();
+        }
+    }
+}
